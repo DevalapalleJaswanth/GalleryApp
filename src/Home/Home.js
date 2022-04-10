44ac6aba2e94d930c9fc43f44e550ac320fccab0
DevalapalleJaswanth <jaswanthreddy040@gmail.com>
2022-04-10 23:32:08 +0530

    made final css changes

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -11,7 +11,7 @@ export default function Home() {
 
   return (
     <div style={{ position: 'relative' }}>
-      <div style={{ position: 'sticky', top: '10px' }}>
+      <div>
         <Search {...myContext} />
       </div>
       <br />
