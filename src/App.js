55332100d137dc4d95f1db07567222d0150aa46c
DevalapalleJaswanth <jaswanthreddy040@gmail.com>
2022-04-10 13:21:57 +0530

    created services to get data

--- src/App.js
+++ src/App.js
@@ -6,15 +6,30 @@ const context = createContext(); // Context is created.
 
 export default function App() {
   const [data, setData] = useState();
+
+  async function getData() {
+    let response = await Services.getImages();
+    if (response.photo) {
+      let temp = response.photo.map((ele, i) => {
+        return {
+          ...ele,
+          url: `https://farm${ele.farm}.staticflickr.com/${ele.server}/${ele.id}_${ele.secret}.jpg`,
+        };
+      });
+      setData([...temp]);
+    }
+  }
+
   useEffect(() => {
-    let response = Services.getImages();
-    setData({ ...response.data });
-    console.log(data);
+    getData();
   }, []);
+
   return (
     <div>
+      {console.log(data)}
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
+      <img src={data && data[0].url} />
     </div>
   );
 }
--- src/Services/Services.js
+++ src/Services/Services.js
@@ -1,12 +1,27 @@
 import axios from 'axios';
 
 async function getImages() {
-  let response = await axios.get(
-    `https://api.flickr.com/services/rest/?method=flickr.galleries.getPhotos&api_key=21c1c693f8ca2a2fed55d4bf9536fa2e&gallery_id=66911286-72157647277042064&format=json&nojsoncallback=1`
-  );
-  let result = response.data;
-  console.log(result.photos);
-  return result;
+  try {
+    let response = await axios.get(
+      `https://api.flickr.com/services/rest/?method=flickr.galleries.getPhotos&api_key=21c1c693f8ca2a2fed55d4bf9536fa2e&gallery_id=66911286-72157647277042064&format=json&nojsoncallback=1`
+    );
+    let result = response.data;
+    //console.log(result.photos);
+    return result.photos;
+  } catch (error) {
+    if (error.response) {
+      console.log(error.response.data);
+      console.log(error.response.status);
+      console.log(error.response.headers);
+      return error.response;
+    } else if (error.request) {
+      console.log(error.request);
+      return error.request;
+    } else {
+      console.log('Error', error.message);
+      return error.message;
+    }
+  }
 }
 
 export default { getImages };
