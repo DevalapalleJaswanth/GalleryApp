97beed31d4e051e5abcd766857a94b6bde7f24b1
DevalapalleJaswanth <jaswanthreddy040@gmail.com>
2022-04-10 12:31:38 +0530

    2 files modified, 2 added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
-import React from "react";
-import "./style.css";
+import React, { createContext, useEffect, useState } from 'react';
+import Services from './Services';
+import './style.css';
+
+const context = createContext(); // Context is created.
 
 export default function App() {
+  const [data, setData] = useState();
+  useEffect(() => {
+    let response = Services.getImages();
+    setData({ ...response.data });
+    console.log(data);
+  }, []);
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
