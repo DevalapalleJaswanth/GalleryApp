690ebeaf080f82f6882bcedef3ea9fb494cdb153
DevalapalleJaswanth <jaswanthreddy040@gmail.com>
2022-04-10 19:34:19 +0530

    made some css changes

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -2,9 +2,11 @@ import React, { useSate } from 'react';
 import { GalleryContext } from './Context';
 import Services from '../Services';
 import TextField from '@mui/material/TextField';
+import './Search.css';
 export default function Search({ setData }) {
   const handleChange = async (e) => {
-    let response = await Services.getImages(e.target.value);
+    let search = e.target.value !== '' ? e.target.value : 'human';
+    let response = await Services.getImages(search);
     if (response.photo) {
       let temp = response.photo.map((ele, i) => {
         return {
@@ -16,8 +18,10 @@ export default function Search({ setData }) {
     }
   };
   return (
-    <div>
+    <div className="center">
       <TextField
+        xs={12}
+        sm={8}
         fullWidth
         label="Search"
         id="Search"
